dd29e96cb65a810afc35ecfb0cb9c3024e93d9d4
choi-PC\choi <braverokmc79@gmail.com>
2016-12-19 15:23:03 +0900

    sql 변경

--- web04/src/test/sql/myproject.sql
+++ web04/src/test/sql/myproject.sql
@@ -2,51 +2,24 @@
 
 CREATE TABLE connecter_info
 (
-   ano       int(11),
+   ano       int(11) AUTO_INCREMENT PRIMARY KEY ,
    m_addr    varchar(30),
    m_port    varchar(10),
    m_agent   varchar(300)
-)
-
-ALTER TABLE connecter_info
-   ADD PRIMARY KEY`primary`(ano)
-
+);
 
 
 
 CREATE TABLE guestbook
 (
-   idx         int(11),
+   idx         int(11) AUTO_INCREMENT PRIMARY KEY ,
    name        varchar(50),
    email       varchar(50),
    passwd      varchar(50),
    content     text,
-   post_date   timestamp DEFAULT 'CURRENT_TIMESTAMP'
-)
+   post_date   timestamp DEFAULT now()
+);
 
-ALTER TABLE guestbook
-   ADD PRIMARY KEY`primary`(idx)
-
-
-CREATE TABLE tbl_member
-(
-   userid       varchar(50),
-   userpw       varchar(50),
-   username     varchar(50),
-   email        varchar(100),
-   regdate      timestamp DEFAULT 'CURRENT_TIMESTAMP',
-   updatedate   timestamp DEFAULT '0000-00-00 00:00:00'
-)
-
-ALTER TABLE tbl_member
-   ADD PRIMARY KEY`primary`(userid)
-
-
-CREATE TRIGGER tbl_member_updatedate_insert
-   BEFORE INSERT
-   ON tbl_member
-   FOR EACH ROW
-SET new.updatedate = now()
 
 
 
